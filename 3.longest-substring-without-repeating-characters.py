be37638a4f2f080e57f968d7f81e72680bca6110
Raymond Tan <ray.tangent42@gmail.com>
2020-12-06 18:02:52 -0500

    fun question.

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,60 @@
+#
+# @lc app=leetcode id=3 lang=python3
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+class Solution:
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     """ Strategy 1: hash + sliding window
+    #     Runtime: O(n)
+    #     Space: O(min(m,n)), where m is the size of the charset and n is the size of the string
+    #     Args:
+    #         s (str): a string
+
+    #     Returns:
+    #         int: return the length longest substring without duplciates characters
+    #     """
+
+    #     n = len(s)
+    #     if n == 0:
+    #         return 0
+
+    #     unique = set()
+    #     i = j = longest = 0
+    #     while j < n:
+    #         if s[j] in unique:
+    #             unique.remove(s[i])
+    #             i += 1
+    #         else:
+    #             unique.add(s[j])
+    #             j += 1
+    #             longest = max(j - i, longest)
+    #     return longest
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """Strategy 2: Sliding window optimized
+        Rutime: O(n)
+        Space: O(min(m,n)), where m is the size of the charset and n is the length of the string
+
+        Args:
+            s (str): a string
+
+        Returns:
+            int: the length of the longest substring without duplicates characters.
+        """
+        n = len(s)
+        if n == 0:
+            return 0
+        char_set = {}
+        i = longest = 0
+        for index, c in enumerate(s):
+            if c in char_set:
+                i = max(char_set[c], i)
+            char_set[c] = index+1
+            longest = max(longest, index - i + 1)
+            # print(i, index, char_set)
+
+        return longest
+# @lc code=end
